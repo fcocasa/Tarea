2a5f4c9d55eb0c539033d8a8d4f1d444bdf83753
martin.borba <m.borba98@gmail.com>
2020-04-05 14:26:43 -0300

    Revert "programa 2 funca"
    This reverts commit 1acb481104eb8e95af4732bed19842fa6c026af3.

--- Tarea/src/programas/programa2.py
+++ Tarea/src/programas/programa2.py
@@ -2,35 +2,8 @@
 import re
 import sys
 
-#import programa1
-#no funciona
-
 def programa(texto):
-    #paso el texto por el programa 1 para borrarle los comentarios
-    texto = re.sub("( )*\*\*\*(.|\\n)*\*\*\*(\\n)*", '', texto)
-    texto = re.sub("( )*//(.)*\\n", '', texto)
-
-    i=0
-    sis = re.findall('( )*si ', texto, flags=0)
-    for x in sis:
-        i = i+1
-
-    k=0
-    pcs = re.findall('( )*para cada ', texto, flags=0)
-    for x in pcs:
-        k = k+1
-
-    j=0 #reconoce tambien los "para cada"
-    paras = re.findall('( )*para ', texto, flags=0)
-    for x in paras:
-        j = j+1
-    j = j-k #para descontar los "para cada"
-
-    l=0
-    mientrass = re.findall('( )*mientras ', texto, flags=0)
-    for x in mientrass:
-        l = l+1
-    texto = "si: " + str(i) + "\npara: " + str(j) + "\npara-cada: " + str(k) + "\nmientras: " + str(l)
+    # Implementar programa
     return texto 
 
 if __name__ == '__main__':
